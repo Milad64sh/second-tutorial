65b1eb5239eedd4ef40912ecd6ddb11abe637713
Milad64sh <m.shalikarian@gmail.com>
2022-06-26 10:45:56 -0700

    second commit

---
+++ src/bookData.js
@@ -0,0 +1,23 @@
+const books = [
+  {
+    id: 1,
+    book: 'book number one',
+    author: 'john doe',
+  },
+  {
+    id: 2,
+    book: 'book number two',
+    author: 'bobby doe',
+  },
+  {
+    id: 3,
+    book: 'book number three',
+    author: 'peter doe',
+  },
+  {
+    id: 4,
+    book: 'book number four',
+    author: 'peter doe',
+  },
+];
+export default books;
