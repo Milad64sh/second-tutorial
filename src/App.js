969f6738f72f12adf3c782f76f902ac94a0e946f
Milad64sh <m.shalikarian@gmail.com>
2022-06-29 07:53:10 -0700

    new tut first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import BookList from './BookList';
-import './index.css';
-const App = () => (
-  <section>
-    <BookList />
-  </section>
-);
+import React, { Component } from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
+const Person = ({ img, name, age }) => {
+  return (
+    <article>
+      <img src={img} alt='person' />
+      <h4>name : {name}</h4>
+      <h4>age : {age}</h4>
+    </article>
+  );
+};
+Person.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+  age: PropTypes.number,
+};
 
-export default App;
+class PersonList extends Component {
+  state = {
+    people: [
+      {
+        id: 1,
+        img: 'https://randomuser.me/api/portraits/thumb/men/75.jpg',
+        name: 'john',
+        age: 24,
+      },
+      {
+        id: 2,
+        img: 'https://randomuser.me/api/portraits/thumb/men/73.jpg',
+        name: 'bob',
+        age: 34,
+      },
+    ],
+  };
+  render() {
+    return (
+      <section>
+        {this.state.people.map((person) => (
+          <Person
+            key={person.id}
+            img={person.img}
+            name={person.name}
+            age={person.age}
+          />
+        ))}
+      </section>
+    );
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return <PersonList />;
+  }
+}
